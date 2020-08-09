76db216af7325c0d8c0413b8535afa53a17727ad
konegishi <k.negishi0705@gmail.com>
2020-08-09 16:27:06 +0900

    データ形式の変更

--- app/setupData.R
+++ app/setupData.R
@@ -14,22 +14,27 @@ num1 <-
 
 # 2桁まで
 num2 <- 
-  expand.grid(str_pad(1:99, 2, pad = "0"), str_pad(1:99, 2, pad = "0")) %>% 
+  expand.grid(1:99, 1:99) %>% 
   as_tibble() %>% 
-  mutate(ans = as.numeric(Var1) + as.numeric(Var2),
-         ansOne = as.numeric(str_sub(Var1, start = -1, end = -1)) + as.numeric(str_sub(Var2, start = -1, end = -1)), 
-         ansTens = as.numeric(str_sub(Var1, start = -2, end = -2)) + as.numeric(str_sub(Var2, start = -2, end = -2)), 
-         isCarried = ansOne >= 10 | ansTens >= 10)
+  mutate(paddedVar1 = str_pad(Var1, 2, pad = "0"),  # 0埋め(padding)した値 
+         paddedVar2 = str_pad(Var2, 2, pad = "0"), 
+         ans = Var1 + Var2,  # 足し算の解答
+         ansOne = as.numeric(str_sub(paddedVar1, start = -1, end = -1)) + as.numeric(str_sub(paddedVar2, start = -1, end = -1)),  # 一の位の足し算解答
+         ansTens = as.numeric(str_sub(paddedVar1, start = -2, end = -2)) + as.numeric(str_sub(paddedVar2, start = -2, end = -2)),  # 十の位の足し算解答
+         isCarried = ansOne >= 10 | ansTens >= 10)  # 繰り上がりしているかどうかのフラグ（TRUE: 繰り上がりあり）
+
 
 # 3桁まで
 num3 <- 
-  expand.grid(str_pad(1:999, 3, pad = "0"), str_pad(1:999, 3, pad = "0")) %>% 
+  expand.grid(1:999, 1:999) %>% 
   as_tibble() %>% 
-  mutate(ans = as.numeric(Var1) + as.numeric(Var2),
-         ansOne = as.numeric(str_sub(Var1, start = -1, end = -1)) + as.numeric(str_sub(Var2, start = -1, end = -1)), 
-         ansTens = as.numeric(str_sub(Var1, start = -2, end = -2)) + as.numeric(str_sub(Var2, start = -2, end = -2)), 
-         ansHundreds = as.numeric(str_sub(Var1, start = -3, end = -3)) + as.numeric(str_sub(Var2, start = -3, end = -3)), 
-         isCarried = ansOne >= 10 | ansTens >= 10 | ansHundreds >= 10)
+  mutate(paddedVar1 = str_pad(Var1, 3, pad = "0"),   # 0埋め(padding)した値 
+         paddedVar2 = str_pad(Var2, 3, pad = "0"), 
+         ans = Var1 + Var2,  # 足し算の解答
+         ansOne = as.numeric(str_sub(paddedVar1, start = -1, end = -1)) + as.numeric(str_sub(paddedVar2, start = -1, end = -1)),  # 一の位の足し算解答
+         ansTens = as.numeric(str_sub(paddedVar1, start = -2, end = -2)) + as.numeric(str_sub(paddedVar2, start = -2, end = -2)),  # 十の位の足し算解答
+         ansHundreds = as.numeric(str_sub(paddedVar1, start = -3, end = -3)) + as.numeric(str_sub(paddedVar2, start = -3, end = -3)),  # 百の位の足し算解答
+         isCarried = ansOne >= 10 | ansTens >= 10 | ansHundreds >= 10)  # 繰り上がりしているかどうかのフラグ（TRUE: 繰り上がりあり）
 
 
 # 保存 ----------------------------------------------------------------------
