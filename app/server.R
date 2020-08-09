d8101559ad499369d23961505ccb800f971236b9
konegishi <k.negishi0705@gmail.com>
2020-08-09 14:20:02 +0900

    2列の問題形式を表示できるように

--- app/server.R
+++ app/server.R
@@ -17,15 +17,113 @@ shinyServer(function(input, output) {
 
 
 # 設定ページのUI ----------------------------------------------------------------
-  output$calSettings <- renderUI({
-    h2("計算プリントの設定")
-  })
+  # output$calSettings <- renderUI({
+  #   h2("計算プリントの設定")
+  # })
+
 
   
 
+# 問題の選択 -------------------------------------------------------------------
+  ansTable <- reactive({
+    # print(input&radioCal)
+    switch(input$digitNum,
+           "1" = readRDS(file = "data/num1.rds"),
+           "2" = readRDS(file = "data/num2.rds"),
+           "3" = readRDS(file = "data/num2.rds"))
+  })
+  
+  hoge <- "hoge"
+  
+  var1 <- reactive({
+    return(ansTable()$Var1[1])
+  })
+  
 # 生成したプリントのUI -------------------------------------------------------------
   output$calPrint <- renderUI({
-    h2("名前")
-    HTML(glue("<div><p>fuga</p></div><span>hoge</span>"))
+    withTags({
+      div(class = "main",
+          p(class = "name", 
+            "なまえ："),
+          
+          # 左列
+          div(class = "column",
+              ul(
+                li("(1) {var1()[1]} + {var2()[1]} = "),
+                li(glue("(2) {var1()[1]}")),
+                li("(3)"),
+                li("(4)"),
+                li("(5)"),
+                li("(6)"),
+                li("(7)"),
+                li("(8)"),
+                li("(9)"),
+                li("(10)")
+              )
+          ),
+          
+          # 右列
+          div(class = "column",
+              ul(
+                li("(11)"),
+                li("(12)"),
+                li("(13)"),
+                li("(14)"),
+                li("(15)"),
+                li("(16)"),
+                li("(17)"),
+                li("(18)"),
+                li("(19)"),
+                li("(20)")
+              )
+          )
+      )
+    })
   })
+  
+  
+  # output$calPrint <- renderUI({
+  #   withTags({
+  #     div(class = "main",
+  #         h3("なまえ："),
+  #         fluidRow(
+  #           column(6,
+  #                  withTags({
+  #                    div(class = "leftColumn",
+  #                        ul(
+  #                          li("(1)"),
+  #                          li("(2)"),
+  #                          li("(3)"),
+  #                          li("(4)"),
+  #                          li("(5)"),
+  #                          li("(6)"),
+  #                          li("(7)"),
+  #                          li("(8)"),
+  #                          li("(9)"),
+  #                          li("(10)")
+  #                        )
+  #                        )
+  #                  })
+  #           ),
+  #           column(6,
+  #                  withTags({
+  #                    div(class = "rightColumn",
+  #                        ul(
+  #                          li("(11)"),
+  #                          li("(12)"),
+  #                          li("(13)"),
+  #                          li("(14)"),
+  #                          li("(15)"),
+  #                          li("(16)"),
+  #                          li("(17)"),
+  #                          li("(18)"),
+  #                          li("(19)"),
+  #                          li("(20)")
+  #                          )
+  #                        )
+  #                  })
+  #           )))
+  #   })
+  # })
 })
+
--- app/ui.R
+++ app/ui.R
@@ -9,12 +9,37 @@ library(shinycustomloader)
 
 # UI
 shinyUI(fluidPage(
+  tags$head(tags$link(rel = "stylesheet", type = "text/css", href = "styles.css")),
   navbarPage(
     title = "算数プリントを自動生成",
     theme = shinytheme("flatly"),
     
-    tabPanel("設定",
-             uiOutput("calSettings")),
+    tabPanel("問題を生成",
+             h2("計算プリントの設定"),
+             checkboxInput('header', 'Header', TRUE),
+             
+             # 四則演算の選択
+             selectInput("radioCal", label = h3("四則演算"),
+                          choices = list("足し算" = 1, 
+                                         "引き算" = 2,
+                                         "掛け算" = 3,
+                                         "割り算" = 4),
+                          selected = 1),
+
+             # 桁数の選択
+             selectInput("digitNum", label = h3("桁数の設定"),
+                         choices = list("1桁のみ" = 1, 
+                                        "2桁まで" = 2,
+                                        "3桁まで" = 3),
+                         selected = 1),
+             
+             # 足し算の設定
+             radioButtons("radioAddition", label = h3("足し算の詳細設定"),
+                          choices = list("繰り上がりあり" = 1, 
+                                         "繰り上がりなし" = 2,
+                                         "ランダム" = 3),
+                          selected = 3)
+             ),
     
     tabPanel("生成されたプリント",
              uiOutput("calPrint"))
